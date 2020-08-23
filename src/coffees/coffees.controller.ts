27d937a1db13065bcb3432570f8abfc227964412
Matthieu Pernelle <matthieu.pernelle@gmail.com>
2020-08-23 18:42:03 +0200

    :sparkles: (route params)

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavores')
+  @Get()
   findAll() {
     return 'This action return all coffes';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
   }
 }
